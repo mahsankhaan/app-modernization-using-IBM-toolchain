fdd9e194019a5b12958325e65a3799cef9a10f44
Muhammad Ahsan Khan <56512649+mahsankhaan@users.noreply.github.com>
2020-01-10 20:08:36 +0500

    Update index.php

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ body, h1,h2,h3,h4,h5,h6 {font-family: "Montserrat", sans-serif}
   <header class="w3-container w3-padding-32 w3-center w3-black" id="home">
     <h1 class="w3-jumbo"><span class="w3-hide-small">I'm</span> Muhammad Ahsan.</h1>
     <p>Photographer and Web Designer.</p>
-    <img src="/me.jpeg" alt="boy" class="w3-image" width="992" height="1108">
+    <img src="/user.png" alt="boy" class="w3-image" width="992" height="1108">
   </header>
 
   <!-- About Section -->
